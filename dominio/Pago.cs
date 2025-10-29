57ccce7515ae6a5a1a3bff75b096f284473329e2
aaguilera0 <alejandroeaguilera0@gmail.com>
2025-10-28 23:44:52 -0300

    feat: agregar clase Pago y modificar clase Pedido

---
+++ dominio/Pago.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    class Pago
+    {
+        public int idPago { get; set; }
+        public int idCliente { get; set; }
+        public int idCarro { get; set; }
+        public string metodoDePago { get; set; }
+        public float monto { get; set; }
+        public DateTime fechaPago { get; set; }
+        public bool estadoPago { get; set; }
+    }
+}
--- dominio/Pedido.cs
+++ dominio/Pedido.cs
@@ -8,5 +8,13 @@ namespace dominio
 {
     public class Pedido
     {
+        public int idPedido { get; set; }
+        public int idCLiente { get; set; }
+        public DateTime fechaCreacion { get; set; }
+        List<detallePedido> detallePedido;
+        public bool estadoPedido { get; set; }
+        public string direccionEntrega { get; set; }
+        public float montoTotal { get; set; }
+        public Pago pago_ { get; set; }
     }
 }
