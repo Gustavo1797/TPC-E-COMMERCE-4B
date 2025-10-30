0423ca97bdf40783ce96538d1ef3e5a86347b5db
aaguilera0 <alejandroeaguilera0@gmail.com>
2025-10-29 23:47:41 -0300

    feat: agregar clase Carrito y Marca

---
+++ dominio/Carrito.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    class Carrito
+    {
+        public int idCarro { get; set; }
+        public int idCliente { get; set; }
+        public int cantidad { get; set; }
+        public float precioUnitario { get; set; }
+        public int items { get; set; }
+        public float total { get; set; }
+        public DateTime fechaCreacion { get; set; }
+        public Boolean estado { get; set; }
+    }
+}
